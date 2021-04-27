bdd4da00054aacc4f6aa23140a67866805bb7f7b
dsrp_ia-Luis-Fernando-117 <464422lb@eur.nl>
2021-04-27 13:01:13 +0200

    Updated scripts with the Bearer token properly installed as an environment variable and with successful API test

--- collect_data.R
+++ collect_data.R
@@ -3,26 +3,19 @@ library("httr")
 library("jsonlite")
 library("tidyverse")
 
-# add any other packages you might need to use
+################################################
 
-# -------- Section 1: Test API --------
-################################################
-################################################
-#
-# test if you can connect to the API
-#
-################################################
+# -------- Step 1: Test API connection --------
+
 ################################################
 
 # if you have correctly set your bearer token as an environment variable, 
 # this retrieved the value of the token and assigns it to "bearer_token"
-bearer_token <- Sys.getenv("
-AAAAAAAAAAAAAAAAAAAAAPxqOgEAAAAAWSDCu%2B1SO943pQCTHA1nM3GAHHQ%3DtoLduLkkrxZeIMgYJ9pf5kcEiRaNrgyuTLZyJWIzPVgTF1EZK0
-")
+bearer_token <- Sys.getenv("BEARER_TOKEN")
+
 # if you didn't manage to create the environment variable, then copy paste the 
 # token below and comment out the line
-bearer_token <- "AAAAAAAAAAAAAAAAAAAAAPxqOgEAAAAAWSDCu%2B1SO943pQCTHA1nM3GAHHQ%3DtoLduLkkrxZeIMgYJ9pf5kcEiRaNrgyuTLZyJWIzPVgTF1EZK0
-"
+
 
 # the authorization header is composed of the text Bearer + space + the token
 headers <- c(Authorization = paste0('Bearer ', bearer_token))
@@ -35,6 +28,7 @@ source("f_aux_functions_Luis.R")
 # you should now see f_test_API and f_test_token_API in the Environment pane
 # type ?source in the console to learn more
 
+f_test_token_API()
 f_test_API(use_header = headers)
 
 # if you want to use the test functions, you need to uncomment the two lines above
@@ -68,11 +62,7 @@ f_test_API(use_header = headers)
 # to process the results from the API, you can collect more observations
 
 ################################################
-################################################
-#
 # Save the data in the assignment repository
-#
-################################################
 ################################################
 
 # save the "raw" dataset -> commit -> push to your assignment repository 
@@ -81,16 +71,14 @@ f_test_API(use_header = headers)
 #			2. let me know and I’ll set up another way for you to share data
 
 
+################################################
+
 # --------- SECTION 2 : Analyses ------------
 
+################################################
 
-# ---  Based on Ana Twitter collection script # -----
-
-rm(list=ls())
-
-# there are 2 packages that can be used to access Twitter data
-#install.packages("twitteR")
-#install.packages("rtweet")
+# ---  TEST DATASET FOR STEP 3 --
+# just 100 observations but that are representative of the tweets I'll analyze
 
 library(twitteR)
 library(rtweet)
