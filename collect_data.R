1224e5d7ae924ea52549c525351fce07419a2e4c
dsrp_ia-Luis-Fernando-117 <464422lb@eur.nl>
2021-05-02 15:34:57 +0200

    Managed to get the user id of the first account I will analyze

--- collect_data.R
+++ collect_data.R
@@ -10,32 +10,21 @@ library("rtweet")
 # -------- Step 1: Test API connection --------
 
 ################################################
-
-# if you have correctly set your bearer token as an environment variable, 
-# this retrieved the value of the token and assigns it to "bearer_token"
 bearer_token <- Sys.getenv("BEARER_TOKEN")
+if(is.null(bearer_token)) {
+  cat("The bearer token is empty. Fix this before you continue!")
+} else {
+  cat("The bearer token has a value. Let's see if it's the correct value.\n")
+  headers <- c(Authorization = paste0('Bearer ', bearer_token))
+  source("f_aux_functions_Luis.R")
+  f_test_API(use_header = headers)
+  # put the headers in a list so it doesn't show up on screen
+  my_header <- list(headers = headers)
+  remove(headers)
+  remove(bearer_token)
+  remove(f_test_API)
+}
 
-# if you didn't manage to create the environment variable, then copy paste the 
-# token below and comment out the line
-
-
-# the authorization header is composed of the text Bearer + space + the token
-headers <- c(Authorization = paste0('Bearer ', bearer_token))
-
-# f_aux_functions.R is in the same directory as collect_data.R, which is the
-# same as the working directory
-# f_aux_functions.R contains two functions that you can use to test the token
-# source("f_aux_functions.R") brings these in the current workspace 
-source("f_aux_functions_Luis.R")
-# you should now see f_test_API and f_test_token_API in the Environment pane
-# type ?source in the console to learn more
-
-f_test_token_API()
-f_test_API(use_header = headers)
-
-# if you want to use the test functions, you need to uncomment the two lines above
-# you can also take a look at examples/example_collect_all_tweets_from_one_user.R 
-#		for another way of testing if you can connect to the API
 
 ################################################
 ################################################
@@ -76,13 +65,13 @@ f_test_API(use_header = headers)
 # --- Preparations for Data extraction ---
 
 # Set up app and keys
-  api_key <-      Sys.getenv("API_KEY")
-  api_secret <-   Sys.getenv("API_SECRET")
-  token <-        Sys.getenv("TOKEN")
-  token_secret <- Sys.getenv("TOKEN_SECRET")
+  #api_key <-      Sys.getenv("API_KEY")
+  #api_secret <-   Sys.getenv("API_SECRET")
+  #token <-        Sys.getenv("TOKEN")
+  #token_secret <- Sys.getenv("TOKEN_SECRET")
 
 # Twitter authentication using the twitteR package function
-  setup_twitter_oauth(api_key, api_secret, token, token_secret)
+  #setup_twitter_oauth(api_key, api_secret, token, token_secret)
 
 # Sets up twitter authentication using the rtweet package function
   token <- create_token(app = "dfcp", 
@@ -115,11 +104,11 @@ f_test_API(use_header = headers)
   
   # Think of the stoping criteria , how many tweets are enough
   
-  
+ 
   ################################################
   # Step 1: collect the user_id for this handle
   ################################################
-  handle <- '@lopezobrador_'
+  handle <- 'lopezobrador_'
   url_handle <- paste0('https://api.twitter.com/2/users/by?usernames=', handle)
   response <-	httr::GET(url = url_handle,
                         config = httr::add_headers(.headers = my_header[["headers"]]))
