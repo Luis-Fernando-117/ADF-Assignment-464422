1f77057c55328dd7548f582bf67613f08f55642b
dsrp_ia-Luis-Fernando-117 <464422lb@eur.nl>
2021-05-08 16:50:30 +0200

    Fixing loops so they dont crash by teh 15 minute limit

--- Process_data.R
+++ Process_data.R
@@ -39,26 +39,57 @@ followers.cleaned.bukele  <- (lst_clean_ids[[4]])
 
 ## ----Get a loop to get statuses of all the cleaned followers
 
-# Create a vector with filtered followers id's
-v.filtered.followers.ids <- as.vector(as.numeric(obrador.followers.cleaned$user_id))
-
-# Get columns names of what "get_timeline" function extracts and create an empty data table
-df.followers.tm <- data.frame(matrix(ncol = 90, nrow = 0))
-my.col.names <- colnames(timeline.obrador)
-colnames(df.followers.tm) <- my.col.names
+# Create an empty data frame
+df.followers.tm.obrador <- data.frame(matrix(ncol = 90, nrow = 0))
 
 
-# Create a loop that gets some tweets of the followers fo the target user
-for (i in v.followers.ids) {
+# Create a loop that gets some tweets of the followers fo the target users
+
+#-- Lopez Obrador -- 
+
+# Given the max amout of tweets you can request per 15 minutes, we 
+# split the follower id's into 4 chunks 
+
+followers.cleaned.obrador_1 <- slice(followers.cleaned.obrador, 1:600)
+followers.cleaned.obrador_2 <- slice(followers.cleaned.obrador, 601:1200)
+followers.cleaned.obrador_3 <- slice(followers.cleaned.obrador, 1201:1800)
+followers.cleaned.obrador_4 <- slice(followers.cleaned.obrador, 1801:2162)
+
+# 1st chunk
+for (i in followers.cleaned.obrador_1) {
   follower_id <- as.numeric(i)
-  df.follower_tm <- as.data.frame((get_timeline(follower_id, n=5)))
-  df.followers.tm <- rbind(df.followers.tm, df.follower_tm)
+  df.follower.tm <- as.data.frame((get_timeline(follower_id, n=30)))
+  df.followers.tm.obrador <- rbind(df.followers.tm.obrador, df.follower.tm)
 }
+
+#2nd chunk
+for (i in followers.cleaned.obrador_2) {
+  follower_id <- as.numeric(i)
+  df.follower.tm <- as.data.frame((get_timeline(follower_id, n=30)))
+  df.followers.tm.obrador <- rbind(df.followers.tm.obrador, df.follower.tm)
+}
+
+#3rd chunk
+for (i in followers.cleaned.obrador_3) {
+  follower_id <- as.numeric(i)
+  df.follower.tm <- as.data.frame((get_timeline(follower_id, n=30)))
+  df.followers.tm.obrador <- rbind(df.followers.tm.obrador, df.follower.tm)
+}
+
+# Last chunk
+for (i in followers.cleaned.obrador_4) {
+  follower_id <- as.numeric(i)
+  df.follower.tm <- as.data.frame((get_timeline(follower_id, n=30)))
+  df.followers.tm.obrador <- rbind(df.followers.tm.obrador, df.follower.tm)
+}
+
+
+
 
 # Filter the observations to include only those tweets that are replies to tweets 
 # made by our target
-dt.followers.replies.to.target <- as.data.table(df.followers.tm)
-dt.followers.replies.to.target <- dt.followers.replies.to.target[reply_to_user_id == targettwitteruserid, ]
+dt.followers.replies.to.obrador <- as.data.table(df.followers.tm)
+dt.followers.replies.to.obrador <- dt.followers.replies.to.obrador[reply_to_user_id == targettwitteruserid, ]
 
 #It works til here !!!!
 
