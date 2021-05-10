b91e0c6208b4abbe4b1fac8d3279c6bf736b9db5
dsrp_ia-Luis-Fernando-117 <464422lb@eur.nl>
2021-05-10 12:55:40 +0200

    Cleaning dataframes of presidents and twitter followers

--- Process_data.R
+++ Process_data.R
@@ -23,11 +23,37 @@ load("timeline.pinera.RData")
 load("timeline.lenin.RData")
 load("timeline.bukele.RData")
 
+#################################
+# --- Tweets processing 
+################################
 
+# Create a subset of the president's timelines with only columns with relevant info
 
+data.obrador <- subset(timeline.obrador, select = c("text", "display_text_width",
+                                                    "reply_to_status_id","reply_to_user_id",
+                                                    "favorite_count", "retweet_count"))
 
+data.pinera  <- subset(timeline.pinera, select = c("text", "display_text_width",
+                                                    "reply_to_status_id","reply_to_user_id",
+                                                    "favorite_count", "retweet_count"))
 
+data.lenin   <- subset(timeline.lenin, select = c("text", "display_text_width",
+                                                    "reply_to_status_id","reply_to_user_id",
+                                                    "favorite_count", "retweet_count"))
 
+data.bukele  <- subset(timeline.bukele, select = c("text", "display_text_width",
+                                                    "reply_to_status_id","reply_to_user_id",
+                                                    "favorite_count", "retweet_count"))
+
+# From the collected tweets of followers, get only those in spanish and are not re-tweets
+  df.followers.tm.obrador.es <- filter(df.followers.tm.obrador, 
+                                       lang == "es" & is_retweet == "FALSE")
+  df.followers.tm.pinera.es  <- filter(df.followers.tm.pinera,
+                                       lang == "es" & is_retweet == "FALSE")
+  df.followers.tm.lenin.es   <- filter(df.followers.tm.lenin,
+                                       lang == "es" & is_retweet == "FALSE")
+  df.followers.tm.bukele.es  <- filter(df.followers.tm.bukele,
+                                       lang == "es" & is_retweet == "FALSE")
 
 
 #############################
